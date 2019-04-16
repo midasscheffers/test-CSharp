c9097b05fc35ed10f91a22ff0b4cbf81957485e7
Midas Scheffers <midassceffers@gmail.com>
2019-04-16 14:25:22 +0200

    this is a test program

---
+++ first console app/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace first_console_app
+{
+	class Program
+	{
+        public static Rabbit r = new Rabbit(20);
+
+		static void Main(string[] args)
+		{
+            long[] list = r.MakeFiboList(r.age);
+            foreach (long Item in list)
+            {
+                Console.WriteLine(Item);
+            }
+        }
+
+        
+
+
+    }
+
+    
+}
---
+++ first console app/Rabbit.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace first_console_app
+{
+    public class Rabbit
+    {
+        public int age;
+
+        public Rabbit(int _age)
+        {
+            age = _age;
+        }
+
+        public long[] MakeFiboList(int len)
+        {
+            long[] list = new long[len];
+            long lastItem = 1;
+            long newItem = 1;
+            long temp;
+            list[0] = 1;
+            for (int i = 1; i < len; i++)
+            {
+                list[i] = newItem;
+                temp = newItem;
+                newItem = lastItem + newItem;
+                lastItem = temp;
+            }
+
+            return list;
+        }
+    }
+}
